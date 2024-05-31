5a3a9c275ea6582e56bc7b7898dd9012f8a6dcb2
wuhaocheng29 <wu2305790321@gmail.com>
2024-05-31 18:17:01 +0800

    UI

--- app/src/main/java/supersocksr/ppp/android/MainActivity.java
+++ app/src/main/java/supersocksr/ppp/android/MainActivity.java
@@ -1,42 +1,90 @@
 package supersocksr.ppp.android;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 import supersocksr.ppp.android.openppp2.Macro;
 import supersocksr.ppp.android.openppp2.PackageX;
-import supersocksr.ppp.android.openppp2.PathX;
 import supersocksr.ppp.android.openppp2.VPNConfiguration;
 import supersocksr.ppp.android.openppp2.VPNLinkConfiguration;
 
+
+
+
 public class MainActivity extends PppVpnActivity {
+    private EditText server;
+    private EditText static_server;
+    private EditText uuid;
+    private EditText tunip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (!is_reload_activity_by_launcher()) {
             setContentView(R.layout.activity_main);
-
+            server=findViewById(R.id.serverAddress);
+            static_server=findViewById(R.id.staticserver);
+            uuid=findViewById(R.id.guidNumber);
+            tunip=findViewById(R.id.tunIP);
             findViewById(R.id.btn_start).setOnClickListener(v -> vpn_run());
             findViewById(R.id.btn_stop).setOnClickListener(v -> vpn_stop());
         }
+    }
+
+private String readRawIpBypass() {
+        String result = "";
+        try {
+            InputStream is = getResources().openRawResource(R.raw.ip);
+            int length = is.available();
+            byte[] buffer = new byte[length];
+            is.read(buffer);
+            result = new String(buffer, "utf8");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+}
+
+private String readRawDomainBypass() {
+        String result = "";
+        try {
+            InputStream is = getResources().openRawResource(R.raw.domain);
+            int length = is.available();
+            byte[] buffer = new byte[length];
+            is.read(buffer);
+            result = new String(buffer, "utf8");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     protected VPNLinkConfiguration vpn_load() {
         VPNLinkConfiguration config = new VPNLinkConfiguration();
         config.SubnetAddress = "255.255.255.0";
-        config.GatewayServer = "10.0.0.1";
-        config.IPAddress = "10.0.0.211";
+        String ip_addr = tunip.getText().toString().trim();
+        String[] temp;
+        String delimiter = "\\.";
+        temp=ip_addr.split(delimiter);
+        config.GatewayServer = temp[0]+"."+temp[1]+"."+temp[2]+"."+"1";
+        config.IPAddress = ip_addr;
         config.VirtualSubnet = true;
         config.BlockQUIC = false;
         config.StaticMode = true;
         config.FlashMode = false;
-        config.AtomicHttpProxySet = true;
+        config.AtomicHttpProxySet = false;
         config.DnsAddresses.add("8.8.8.8");
         config.DnsAddresses.add("8.8.4.4");
 
-        config.BypassIpListFiles.add(PathX.path_get_cache_dir(MainActivity.this) + "/ip.txt");
+        config.BypassIpList = readRawIpBypass();
 
-        config.DNSRuleList = "baidu.com/1.2.4.8/nic";
+        config.DNSRuleList = readRawDomainBypass();
         config.AllowedApplicationPackageNames.add(PackageX.package_get_package_name(MainActivity.this));
         config.DisallowedApplicationPackageNames.add(PackageX.package_get_package_name(MainActivity.this));
 
@@ -67,7 +115,7 @@ public class MainActivity extends PppVpnActivity {
         vpn.udp.static_.icmp = true;
         vpn.udp.static_.keep_alived[0] = 0;
         vpn.udp.static_.keep_alived[1] = 0;
-        vpn.udp.static_.servers.add("23.26.232.16:20000");
+        vpn.udp.static_.servers.add(static_server.getText().toString().trim());
 
         vpn.websocket.verify_peer = true;
         vpn.websocket.http.error = "Status Code: 404; Not Found";
@@ -80,12 +128,10 @@ public class MainActivity extends PppVpnActivity {
         vpn.websocket.http.request.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0");
         vpn.websocket.http.response.put("Server", "Kestrel");
 
-        vpn.client.guid = "{14261208-BB45-4DEB-B115-0FEA1D91B85B}";
-        vpn.client.server = "ppp://23.26.232.16:20000/";
+        vpn.client.guid = java.util.UUID.randomUUID().toString();
+        vpn.client.server = server.getText().toString().trim();
         vpn.client.bandwidth = 0;
         vpn.client.reconnections.timeout = Macro.PPP_TCP_CONNECT_TIMEOUT;
-        vpn.client.http_proxy.bind = "127.0.0.1";
-        vpn.client.http_proxy.port = Macro.PPP_DEFAULT_HTTP_PROXY_PORT;
 
         return config;
     }
